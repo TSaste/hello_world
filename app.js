cbfa9818986df7b70d7c6addd57e18957fe05bf7
TSaste <tejascodes002@gmail.com>
2022-07-23 23:25:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Setting up random color
+const setColor = () => {
+    setInterval(function () {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.color = "#" + randomColor;
+        // color.innerHTML = "#" + randomColor;
+    }, 800);
+}
+setColor()
